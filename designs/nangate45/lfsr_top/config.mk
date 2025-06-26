81242fa111ea1aef6c251bb4af70a27a62bdde37
Eric Chuang <ericchuang94@gmail.com>
2025-06-26 16:51:08 -0700

    remove pdn config

--- designs/nangate45/lfsr_top/config.mk
+++ designs/nangate45/lfsr_top/config.mk
@@ -1,11 +1,10 @@
 export DESIGN_NAME = lfsr_top
 export PLATFORM    = nangate45
 
-export VERILOG_FILES = $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NAME)/*.v))
-export SDC_FILE      = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
+export VERILOG_FILES = $(sort $(wildcard $(BENCH_DESIGN_HOME)/src/$(DESIGN_NAME)/*.v))
+export SDC_FILE      = $(BENCH_DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
+export ABC_AREA      = 1
 
-export CORE_UTILIZATION ?= 55
+export CORE_UTILIZATION = 20
 export PLACE_DENSITY_LB_ADDON = 0.20
 export TNS_END_PERCENT        = 100
-
-export PDN_TCL = $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/grid_strategy-M1-M4.tcl
