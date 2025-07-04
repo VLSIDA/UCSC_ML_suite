4220b5f6098370997b16d978b96d6e10c534c998
Daniel Qeli <danielqeli1@gmail.com>
2025-07-04 16:43:38 -0700

    Add minimax for sky130, asap7, and nangate45

---
+++ designs/nangate45/minimax/config.mk
@@ -0,0 +1,12 @@
+export DESIGN_NAME	= minimax
+export PLATFORM		= nangate45
+
+export VERILOG_FILES	= $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NAME)/*.v))
+export SDC_FILE			= $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+export PLACE_DENSITY	= 0.7
+export PLACE_PINS_ARGS := -random
+
+export CORE_UTILIZATION	= 60
+export CORE_ASPECT_RATIO	= 1.0
+export CORE_MARGIN		= 6
---
+++ designs/sky130hd/minimax/config.mk
@@ -0,0 +1,12 @@
+export DESIGN_NAME	= minimax
+export PLATFORM		= sky130hd
+
+export VERILOG_FILES	= $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NAME)/*.v))
+export SDC_FILE			= $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+export PLACE_DENSITY	= 0.6
+export PLACE_PINS_ARGS := -random
+
+export CORE_UTILIZATION	= 40
+export CORE_ASPECT_RATIO	= 1.0
+export CORE_MARGIN		= 12
