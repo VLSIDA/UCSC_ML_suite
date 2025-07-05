2b4662efb4ce808370363926605aba0db75bf452
Daniel Qeli <danielqeli1@gmail.com>
2025-07-04 17:36:31 -0700

    Updated DESIGN_NICKNAME to DESIGN_NAME in all config.mk

--- designs/nangate45/minimax/config.mk
+++ designs/nangate45/minimax/config.mk
@@ -2,7 +2,7 @@ export DESIGN_NAME	= minimax
 export PLATFORM		= nangate45
 
 export VERILOG_FILES	= $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NAME)/*.v))
-export SDC_FILE			= $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+export SDC_FILE			= $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
 
 export PLACE_DENSITY	= 0.7
 export PLACE_PINS_ARGS := -random
