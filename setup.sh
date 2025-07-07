d8ccb4c37fff64c076efeb855aba446d893aed37
Matthew Guthaus <mrg@ucsc.edu>
2025-07-07 13:18:19 -0700

    Add shallow submodule update for speed

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git submodule init
-git submodule update --recursive
+git submodule update --recursive --depth 1 --recommend-shallow
 ln -s OpenROAD-flow-scripts/flow/util .
 ln -s OpenROAD-flow-scripts/flow/scripts .
 ln -s OpenROAD-flow-scripts/flow/platforms .
