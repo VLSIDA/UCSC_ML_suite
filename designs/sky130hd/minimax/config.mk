40785ee30d2d45fabe7b18fd084f0c5c38b9bb8f
Daniel Qeli <danielqeli1@gmail.com>
2025-07-08 10:21:20 -0700

    Move setup script to individual design makefiles

--- designs/sky130hd/minimax/config.mk
+++ designs/sky130hd/minimax/config.mk
@@ -1,7 +1,21 @@
 export DESIGN_NAME	= minimax
 export PLATFORM		= sky130hd
 
-export VERILOG_FILES	= $(sort $(wildcard $(DESIGN_HOME)/src/$(DESIGN_NAME)/*.v))
+# This could be a wildcard
+REPO_FILES = $(BENCH_DESIGN_HOME)/src/$(DESIGN_NAME)/repo/rtl/$(DESIGN_NAME).v
+# This cannot be a wildcard, since it is used in the dependency
+export VERILOG_FILES = $(BENCH_DESIGN_HOME)/src/$(DESIGN_NAME)/$(DESIGN_NAME).v
+
+REPO_LICENSE = $(BENCH_DESIGN_HOME)/src/$(DESIGN_NAME)/repo/LICENSE
+LICENSE_FILE = $(BENCH_DESIGN_HOME)/src/$(DESIGN_NAME)/LICENSE
+
+$(VERILOG_FILES): do-setup $(REPO_FILES)
+	echo "Copying $(REPO_LICENSE)  ->  $(LICENSE_FILE)"
+	cp -u $(REPO_LICENSE) $(LICENSE_FILE)
+	echo "Translating $(REPO_FILES)  ->  $@"
+	sv2v -w $@ $(REPO_FILES)
+	echo "Done."
+
 export SDC_FILE			= $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
 
 export PLACE_DENSITY	= 0.6
