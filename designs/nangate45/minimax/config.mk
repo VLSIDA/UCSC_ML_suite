62fa6b7d70b2822f4b358d40f9100b517a5c4c90
Benjamin Goldblatt <bgoldbla@ucsc.edu>
2025-10-13 12:00:38 -0700

    Removing obsolete -random command for PLACE_PIN_ARGS
    Signed-off-by: Benjamin Goldblatt <bgoldbla@ucsc.edu>

--- designs/nangate45/minimax/config.mk
+++ designs/nangate45/minimax/config.mk
@@ -6,7 +6,6 @@ export PLATFORM		= nangate45
 export SDC_FILE			= $(DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
 
 export PLACE_DENSITY	= 0.7
-export PLACE_PINS_ARGS	= -random
 
 export CORE_UTILIZATION	= 60
 export CORE_ASPECT_RATIO	= 1.0
--- designs/sky130hd/minimax/config.mk
+++ designs/sky130hd/minimax/config.mk
@@ -7,7 +7,6 @@ export PLATFORM		= sky130hd
 export SDC_FILE			= $(BENCH_DESIGN_HOME)/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
 
 export PLACE_DENSITY	= 0.6
-export PLACE_PINS_ARGS	= -random
 
 export CORE_UTILIZATION	= 40
 export CORE_ASPECT_RATIO	= 1.0
